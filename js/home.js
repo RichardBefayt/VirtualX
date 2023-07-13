366b304c0192866cc0fd81d0843058a0c25702be
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2023-07-13 19:20:50 +0200

    Sidebar faite, manque Searchbar

---
+++ js/home.js
@@ -0,0 +1,13 @@
+const sidebar = document.querySelector('.sidebar');
+const open = document.querySelector('.open-btn');
+const close = document.querySelector('.close-btn');
+
+open.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    open.style.display = 'none';
+});
+
+close.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+    open.style.display = 'block';
+})
