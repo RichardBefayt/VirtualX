669ec3291d86c64d0e7a1b5464bd440c0442476e
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2023-07-15 18:05:36 +0200

    Carrousel home ok + début présentation produits home

--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
+// import products from "../data/products.json";
+
+// Sidebar
 const sidebar = document.querySelector('.sidebar');
 const open = document.querySelector('.open-btn');
 const close = document.querySelector('.close-btn');
@@ -10,4 +13,22 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     sidebar.classList.remove('active');
     open.style.display = 'block';
-})
+});
+
+// Présentation des produits
+
+  
+// Carrousel
+const cardsContainer = document.querySelector('.cards-container');
+const cards = document.querySelectorAll('.card');
+const cardCount = cards.length;
+
+for (let i = 0; i < cardCount; i++) {
+    const cardClone = cards[i].cloneNode(true);
+    cardsContainer.appendChild(cardClone);
+}
+
+const cardWidth = cards[0].offsetWidth;
+const cardsContainerWidth = cardCount * cardWidth;
+
+cardsContainer.style.width = `${cardsContainerWidth}px`;
