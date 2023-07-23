8844bd86445ea7f147368b82e2814ee91ac0cdbd
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2023-07-23 14:26:05 +0200

    Products en cours

--- js/home.js
+++ js/home.js
@@ -1,18 +1,3 @@
-// ***** Sidebar ***** //
-const sidebar = document.querySelector('.sidebar');
-const open = document.querySelector('.open-btn');
-const close = document.querySelector('.close-btn');
-
-open.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-    open.style.display = 'none';
-});
-
-close.addEventListener('click', () => {
-    sidebar.classList.remove('active');
-    open.style.display = 'block';
-});
-
 // ***** Présentation des produits ***** //
 async function getAllProductData() {
     try {
@@ -87,35 +72,34 @@ function displayRandomProducts(products) {
 
 let allProducts = [];
 
-const categories = ['pc', 'smartphones', 'audio', 'vr'];
+const categories = ['vr'];
 
 function fetchAndShuffleProducts() {
     getAllProductData()
-        .then(data => {
-            for (const category of categories) {
-                const products = data[category];
-                shuffleArray(products);
-                allProducts.push(...products);
-            }
-            shuffleArray(allProducts);
+    .then(data => {
+        for (const category of categories) {
+            const products = data[category];
+            shuffleArray(products);
+            allProducts.push(...products);
+        }
+        shuffleArray(allProducts);
 
-            displayRandomProducts(allProducts);
-        })
-        .catch(error => {
-            console.log('Une erreur est survenue :', error);
-        });
+        displayRandomProducts(allProducts);
+    })
+    .catch(error => {
+        console.log('Une erreur est survenue :', error);
+    });
 }
 
 function displayNextCategory() {
     if (allProducts.length === 0) {
         fetchAndShuffleProducts();
-    } else {
-        setTimeout(() => {
-            displayRandomProducts(allProducts);
+        } else {
+            setTimeout(() => {
+                displayRandomProducts(allProducts);
         }, 10000);
     }
 }
-
 
 // ***** Carrousel ***** //
 const cardsContainer = document.querySelector('.cards-container');
@@ -135,122 +119,3 @@ cardsContainer.style.width = `${cardsContainerWidth}px`;
 window.addEventListener('DOMContentLoaded', () => {
     displayNextCategory();
 });
-
-// ***** Fonctions ***** //
-
-// Fonction pour afficher la page de présentation éclair
-function displayPresentationPage() {
-    setTimeout(() => {
-      const presentationPage = document.querySelector('.global-presentation');
-      presentationPage.style.display = 'none';
-      fetchAndShuffleProducts(); // Appel de la fonction pour afficher les produits aléatoirement après la page de présentation
-    }, 3000); // 3000 ms = 3 secondes de présentation
-  }
-  
-  // Fonction pour récupérer les données des produits
-  async function getAllProductData() {
-    try {
-      const response = await fetch("../data/products.json");
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log('Une erreur est survenue :', error);
-    }
-  }
-  
-  // Fonction pour mélanger un tableau aléatoirement
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
-  
-  // Fonction pour afficher les détails d'un produit
-  function displayProduct(product) {
-    const productName = document.querySelector('.product-title');
-    productName.textContent = product.name;
-  
-    const productDescription = document.querySelector('.product-description');
-    productDescription.textContent = product.description;
-  
-    const productPrice = document.querySelector('.product-price');
-    productPrice.textContent = `${product.price} €`;
-  
-    const containerRightImageContainer = document.querySelector('.pres-products-right');
-    while (containerRightImageContainer.firstChild) {
-      containerRightImageContainer.removeChild(containerRightImageContainer.firstChild);
-    }
-  
-    const divElement = document.createElement("div");
-    const imgElement = document.createElement("img");
-  
-    if (product.img !== "") {
-      imgElement.src = product.img;
-      imgElement.alt = product.name;
-      divElement.appendChild(imgElement);
-    }
-  
-    let containerLeft = document.querySelector(".pres-products-left");
-  
-    while (containerLeft.firstChild) {
-      containerLeft.removeChild(containerLeft.firstChild);
-    }
-  
-    containerLeft.appendChild(productName)
-    containerLeft.appendChild(productDescription)
-    containerLeft.appendChild(productPrice)
-  
-    if (divElement.hasChildNodes()) {
-      let imageParentDiv = document.querySelector(".pres-products-right")
-      imageParentDiv.append(divElement);
-    }
-  }
-  
-  // Fonction pour afficher les produits aléatoirement
-  function displayRandomProducts(products) {
-    if (products.length === 0) {
-      displayNextCategory();
-      return;
-    }
-  
-    const randomProduct = products.pop();
-    displayProduct(randomProduct);
-  
-    setTimeout(() => {
-      displayRandomProducts(products);
-    }, 10000); // 10000 ms = 10 secondes
-  }
-  
-  // Fonction pour récupérer et mélanger les produits
-  function fetchAndShuffleProducts() {
-    getAllProductData()
-      .then(data => {
-        const categories = ['pc', 'smartphones', 'audio', 'vr'];
-        let allProducts = [];
-  
-        for (const category of categories) {
-          const products = data[category];
-          shuffleArray(products);
-          allProducts.push(...products);
-        }
-        shuffleArray(allProducts);
-  
-        displayRandomProducts(allProducts);
-      })
-      .catch(error => {
-        console.log('Une erreur est survenue :', error);
-      });
-  }
-  
-  // ***** Sidebar ***** //
-  // (Votre code Sidebar actuel reste inchangé)
-  
-  // ***** Carrousel ***** //
-  // (Votre code Carrousel actuel reste inchangé)
-  
-  // ***** Appel de la fonction pour afficher la page de présentation éclair ***** //
-  window.addEventListener('DOMContentLoaded', () => {
-    displayPresentationPage();
-  });
-  
---
+++ js/products.js
@@ -0,0 +1,114 @@
+const productsContainer = document.getElementById("products-container");
+let productsData = null;
+
+async function getAllProductData() {
+    try {
+        const response = await fetch("../data/products.json");
+        productsData = await response.json();
+        return productsData;
+    } catch (error) {
+        console.log('Une erreur est survenue :', error);
+        return null;
+    }
+}
+
+// Affiche les cartes de produits
+function displayProductCards(productsData) {
+    const products = productsData.vr;
+
+    productsContainer.textContent = "";
+
+    products.forEach(product => {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+
+        const productCardInner = document.createElement("div");
+        productCardInner.classList.add("product-card-inner");
+
+        const cardHeader = document.createElement("div");
+        cardHeader.classList.add("product-card-header");
+
+        const cardTitle = document.createElement("h3");
+        cardTitle.classList.add("card-title");
+        cardTitle.textContent = product.name;
+
+        const cardMiddle = document.createElement("div");
+        cardMiddle.classList.add("product-card-middle");
+
+        // const cardDescription = document.createElement("p");
+        // cardDescription.classList.add("card-description");
+        // cardDescription.textContent = product.description;
+
+        const cardImgContainer = document.createElement("div");
+        cardImgContainer.classList.add("card-middle-img");
+
+        const cardImg = document.createElement("img");
+        cardImg.classList.add("card-img");
+        cardImg.setAttribute("src", product.img);
+        cardImg.setAttribute("alt", product.name);
+
+        const cardFooter = document.createElement("div");
+        cardFooter.classList.add("product-card-footer");
+
+        const cardPrice = document.createElement("p");
+        cardPrice.classList.add("card-price");
+        cardPrice.textContent = product.price + " €";
+
+        const cardIconsContainer = document.createElement("div");
+        cardIconsContainer.classList.add("card-icons-container");
+
+        const favoriteIcon = document.createElement("i");
+        favoriteIcon.classList.add("card-icon", "fa-regular", "fa-bookmark", "favorite");
+
+        const addToCartIcon = document.createElement("i");
+        addToCartIcon.classList.add("card-icon", "fa-solid", "fa-cart-shopping", "add-to-cart");
+        
+        cardIconsContainer.appendChild(favoriteIcon);
+        cardIconsContainer.appendChild(addToCartIcon);
+        
+        cardHeader.appendChild(cardTitle);
+        // cardMiddle.appendChild(cardDescription);
+        cardImgContainer.appendChild(cardImg);
+        cardMiddle.appendChild(cardImgContainer);
+        cardFooter.appendChild(cardPrice);
+        cardFooter.appendChild(cardIconsContainer);
+        
+        
+        productCardInner.appendChild(cardHeader);
+        productCardInner.appendChild(cardMiddle);
+        productCardInner.appendChild(cardFooter);
+        
+        productCard.appendChild(productCardInner);
+
+        productsContainer.appendChild(productCard);
+    });
+}
+
+// Écouteur d'événement pour le menu déroulant de tri
+const sortMenu = document.getElementById("sort");
+sortMenu.addEventListener("change", (event) => {
+    const selectedSortOrder = event.target.value;
+    sortProducts(selectedSortOrder);
+});
+
+// Trie des produits par prix
+function sortProducts(sortOrder) {
+    if (!productsData) return; // Vérifie si les données des produits sont disponibles
+
+    const products = productsData.vr.slice(); // Fait une copie du tableau des produits
+
+    if (sortOrder === "asc") {
+        products.sort((a, b) => a.price - b.price); // Trie par prix croissant
+    } else if (sortOrder === "desc") {
+        products.sort((a, b) => b.price - a.price); // Trie par prix décroissant
+    }
+
+    displayProductCards({ "vr": products }); // Affiche les produits triés
+}
+
+// Chargement des données depuis le fichier products.json et affichage initial des produits
+getAllProductData().then((data) => {
+    if (data) {
+        displayProductCards(data);
+    }
+});
